a5535b5ddf9af7c189908974499ed36a98745e79
Mariem Mathlouthi <mariemmathlouthi15@gmail.com>
2025-11-21 16:20:51 +0100

    Correction de la fuite mémoire dans ex3/leak.c

--- ex3/leak.c
+++ ex3/leak.c
@@ -6,29 +6,30 @@
 
 int main()
 {
-	int *segments[NUM_SEGMENTS];
+    int *segments[NUM_SEGMENTS];
 
-	for (int i = 0; i < NUM_SEGMENTS; ++i) {
-		segments[i] = malloc(SEGMENT_SIZE * sizeof(int));
-		if (!segments[i]) {
-			printf("Erreur lors de l'allocation de mémoire pour le segment %d.\n", i);
-			for (int j = 0; j < i; ++j) {
-				free(segments[j]);
-			}
-			return -1;
-		}
-	}
+    for (int i = 0; i < NUM_SEGMENTS; ++i) {
+        segments[i] = malloc(SEGMENT_SIZE * sizeof(int));
+        if (!segments[i]) {
+            printf("Erreur lors de l'allocation de mémoire pour le segment %d.\n", i);
+            for (int j = 0; j < i; ++j) {
+                free(segments[j]);
+            }
+            return -1;
+        }
+    }
 
-	for (int i = 0; i < NUM_SEGMENTS; ++i) {
-		for (int j = 0; j < SEGMENT_SIZE; ++j) {
-			segments[i][j] = i + j;
-		}
-	}
+    for (int i = 0; i < NUM_SEGMENTS; ++i) {
+        for (int j = 0; j < SEGMENT_SIZE; ++j) {
+            segments[i][j] = i + j;
+        }
+    }
 
-	for (int i = 0; i < NUM_SEGMENTS; ++i) {
-		free(segments[i]);
-		segments[i++] = NULL;
-	}
+    // Libération correcte de tous les segments
+    for (int i = 0; i < NUM_SEGMENTS; ++i) {
+        free(segments[i]);
+        segments[i] = NULL;
+    }
 
-	return 0;
+    return 0;
 }
