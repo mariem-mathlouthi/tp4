112ee4d8993bd1ae6dd5f9c4134e056d8971e802
Mariem Mathlouthi <mariemmathlouthi15@gmail.com>
2025-11-21 15:28:03 +0100

    Correction du bug mémoire dans apply_laplacian (exerce 1)

--- ex1/main.c
+++ ex1/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
 #include "ppm.h"
 
 // Fonction pour dessiner un carré dans une image PPM
 void draw_square(
-		struct ppm_image *im, int x1, int y1, int x2, int y2,
-		unsigned char r, unsigned char g, unsigned char b)
+                struct ppm_image *im, unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
+                unsigned char r, unsigned char g, unsigned char b)
 {
-	for (int y = y1; y <= y2; ++y) {
-		for (int x = x1; x <= x2; ++x) {
-			ppm_image_setpixel(im, x, y, r, g, b);
-		}
-	}
+    for (unsigned int y = y1; y <= y2; ++y) {
+        for (unsigned int x = x1; x <= x2; ++x) {
+            ppm_image_setpixel(im, x, y, r, g, b);
+        }
+    }
 }
 
 #define NORM(V) ((V) < 0 ? 0 : ((V) > 255 ? 255 : (V)))
@@ -19,83 +18,83 @@ void draw_square(
 // Fonction pour appliquer une convolution avec un laplacien pour la détection de contours
 void apply_laplacian(struct ppm_image *src, struct ppm_image *dest)
 {
-	for (int y = 0; y < src->height; ++y) {
-		for (int x = 0; x < src->width; ++x) {
-			// Calcul de la somme des différences de luminosité autour du pixel
-			int sum_r = -4 * src->px[y * src->width + x].r +
-				src->px[(y - 1) * src->width + x].r +
-				src->px[(y + 1) * src->width + x].r +
-				src->px[y * src->width + x - 1].r +
-				src->px[y * src->width + x + 1].r;
+    for (unsigned int y = 0; y < src->height; ++y) {
+        for (unsigned int x = 0; x < src->width; ++x) {
+            int sum_r = -4 * src->px[y * src->width + x].r;
+            int sum_g = -4 * src->px[y * src->width + x].g;
+            int sum_b = -4 * src->px[y * src->width + x].b;
 
-			int sum_g = -4 * src->px[y * src->width + x].g +
-				src->px[(y - 1) * src->width + x].g +
-				src->px[(y + 1) * src->width + x].g +
-				src->px[y * src->width + x - 1].g +
-				src->px[y * src->width + x + 1].g;
+            if (y > 0) {
+                sum_r += src->px[(y-1)*src->width + x].r;
+                sum_g += src->px[(y-1)*src->width + x].g;
+                sum_b += src->px[(y-1)*src->width + x].b;
+            }
+            if (y < src->height-1) {
+                sum_r += src->px[(y+1)*src->width + x].r;
+                sum_g += src->px[(y+1)*src->width + x].g;
+                sum_b += src->px[(y+1)*src->width + x].b;
+            }
+            if (x > 0) {
+                sum_r += src->px[y*src->width + (x-1)].r;
+                sum_g += src->px[y*src->width + (x-1)].g;
+                sum_b += src->px[y*src->width + (x-1)].b;
+            }
+            if (x < src->width-1) {
+                sum_r += src->px[y*src->width + (x+1)].r;
+                sum_g += src->px[y*src->width + (x+1)].g;
+                sum_b += src->px[y*src->width + (x+1)].b;
+            }
 
-			int sum_b = -4 * src->px[y * src->width + x].b +
-				src->px[(y - 1) * src->width + x].b +
-				src->px[(y + 1) * src->width + x].b +
-				src->px[y * src->width + x - 1].b +
-				src->px[y * src->width + x + 1].b;
-
-			// Normalisation des valeurs
-			unsigned char val_r = (unsigned char)NORM(sum_r);
-			unsigned char val_g = (unsigned char)NORM(sum_g);
-			unsigned char val_b = (unsigned char)NORM(sum_b);
-
-			// Assignation du pixel résultat
-			ppm_image_setpixel(dest, x, y, val_r, val_g, val_b);
-		}
-	}
+            ppm_image_setpixel(dest, x, y,
+                               (unsigned char)NORM(sum_r),
+                               (unsigned char)NORM(sum_g),
+                               (unsigned char)NORM(sum_b));
+        }
+    }
 }
 
 int main()
 {
-	struct ppm_image src_img, dest_img;
-	int width = 800;
-	int height = 600;
+    struct ppm_image src_img, dest_img;
+    unsigned int width = 800;
+    unsigned int height = 600;
 
-	// Initialisation des images
-	if (ppm_image_init(&src_img, width, height))
-		return -1;
-	if (ppm_image_init(&dest_img, width, height)) {
-		ppm_image_release(&src_img);
-		return -1;
-	}
+    // Initialisation des images
+    if (ppm_image_init(&src_img, width, height))
+        return -1;
+    if (ppm_image_init(&dest_img, width, height)) {
+        ppm_image_release(&src_img);
+        return -1;
+    }
 
-	// Draw some cool figures
-	for (int i = 0; i < width; ++i) {
-		for (int j = 0; j < height; ++j) {
-			ppm_image_setpixel(
-				&src_img, i, j,
-				i     % 255,
-				j     % 255,
-				(i+j) % 255);
-		}
-	}
+    // Création de l'image source avec dégradé
+    for (unsigned int i = 0; i < width; ++i) {
+        for (unsigned int j = 0; j < height; ++j) {
+            ppm_image_setpixel(&src_img, i, j, i % 255, j % 255, (i+j) % 255);
+        }
+    }
 
-	for (int i = 0; i < height; ++i)
-		ppm_image_setpixel(&src_img, i, i, 255, 0, 0);
+    // Diagonale rouge
+    for (unsigned int i = 0; i < height; ++i)
+        ppm_image_setpixel(&src_img, i, i, 255, 0, 0);
 
-	// Dessiner un carré rouge sur l'image source
-	draw_square(&src_img, 200, 200, 600, 400, 255, 0, 0);
+    // Dessiner un carré rouge
+    draw_square(&src_img, 200, 200, 600, 400, 255, 0, 0);
 
-	// Enregistrer l'image résultante
-	if (ppm_image_dump(&src_img, "carre.ppm"))
-		fprintf(stderr, "Erreur lors de l'enregistrement de l'image.\n");
+    // Enregistrer l'image source
+    if (ppm_image_dump(&src_img, "carre.ppm"))
+        fprintf(stderr, "Erreur lors de l'enregistrement de l'image.\n");
 
-	// Appliquer une convolution avec un laplacien pour détecter les contours
-	apply_laplacian(&src_img, &dest_img);
+    // Appliquer le Laplacien pour détecter les contours
+    apply_laplacian(&src_img, &dest_img);
 
-	// Enregistrer l'image résultante
-	if (ppm_image_dump(&dest_img, "contours.ppm"))
-		fprintf(stderr, "Erreur lors de l'enregistrement de l'image.\n");
+    // Enregistrer l'image des contours
+    if (ppm_image_dump(&dest_img, "contours.ppm"))
+        fprintf(stderr, "Erreur lors de l'enregistrement de l'image.\n");
 
-	// Libérer la mémoire
-	ppm_image_release(&src_img);
-	ppm_image_release(&dest_img);
+    // Libération mémoire
+    ppm_image_release(&src_img);
+    ppm_image_release(&dest_img);
 
-	return 0;
+    return 0;
 }
